953350413d24311dce2da3e59d46d3d1d5230a6d
EunSeo0117 <gmjea023@gmail.com>
2025-11-19 03:40:52 +0900

    fix : ELB를 위해 프록시설정 제거

--- frontend-express/server.js
+++ frontend-express/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import path from "path";
-import { createProxyMiddleware } from "http-proxy-middleware";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -8,20 +7,9 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 3000;
-const API_TARGET = process.env.API_TARGET || "http://localhost:8080";
 
 const PUBLIC_ROOT = path.join(__dirname, "public");
 const HTML_ROOT = path.join(PUBLIC_ROOT, "html");
-
-app.use(
-  "/api",
-  createProxyMiddleware({
-    target: API_TARGET,
-    changeOrigin: true,
-    pathRewrite: { "^/api": "" },
-    logLevel: "warn",
-  }),
-);
 
 app.get("/", (_, res) => res.sendFile("index.html", { root: PUBLIC_ROOT }));
 app.get("/index", (_, res) => res.sendFile("index.html", { root: PUBLIC_ROOT }));
